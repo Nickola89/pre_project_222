bd6567ab56507bc6c4b87364b6aa616527231685
Nickolay Artemyev <Artemyev1000@gmail.com>
2023-06-08 07:36:37 +0900

    second commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImpl;
 
 import java.util.List;
@@ -19,19 +18,9 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping()
-    public String getCars(ModelMap model) {
-        model.addAttribute("cars", carService.getAllCars());
-        return "cars";
-    }
-
     @GetMapping("/cars")
-    public String getCarLimit(@RequestParam(defaultValue = "0") int count, ModelMap model) {
-        List<Car> cars = carService.getAllCars();
-        if (count > 0 && count <= cars.size()) {
-            cars = cars.subList(0, count);
-        }
-        model.addAttribute("cars", cars);
-        return "cars";
+    public String showCarList(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
+        model.addAttribute("cars", carService.getAllCars(count));
+        return "/cars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,15 +5,22 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CarServiceImpl implements CarService {
-    @Override
-    public List<Car> getAllCars() {
-        List<Car> cars = new ArrayList<>();
+    List<Car> cars = new ArrayList<>();
+
+    {
         cars.add(new Car("model1", 1, "vin1"));
         cars.add(new Car("model2", 2, "vin2"));
         cars.add(new Car("model3", 3, "vin3"));
-        return cars;
+        cars.add(new Car("model4", 4, "vin4"));
+        cars.add(new Car("model5", 5, "vin5"));
+    }
+
+    @Override
+    public List<Car> getAllCars(int count) {
+        return cars.stream().limit(count).collect(Collectors.toList());
     }
 }
